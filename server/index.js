83ad6410d70ad568104882fd1762346f2b5db560
Ivan <madjaricivan@gmail.com>
2026-06-03 14:23:54 +0200

    Update site

--- server/index.js
+++ server/index.js
@@ -89,6 +89,7 @@ app.use(
         scriptSrcAttr: ["'unsafe-inline'"],
         styleSrc     : ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
         fontSrc      : ["'self'", "https://fonts.gstatic.com"],
+        imgSrc       : ["'self'", "data:", "https://images.unsplash.com"],
       },
     },
   })
